2b7b104fdca9ed67eda55a1f7694832310204771
mehmet ali <alimehmet1@windowslive.com>
2021-09-26 18:19:40 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public void Add(Category category)
+        {
+            _categoryDal.Add(category);
+        }
+
+        public void Delete(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core/DataAccess/EfEntityRepositoryBase.cs
@@ -0,0 +1,64 @@
+﻿using Core.Entities.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    public class EfEntityRepositoryBase<Tentity, TContext> : IEntityRepository<Tentity>
+        where Tentity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+        public void Add(Tentity entity)
+        {
+            using (TContext db= new TContext())
+            {
+                var addedEntity = db.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                db.SaveChanges();
+            }
+        }
+
+        public void Delete(Tentity entity)
+        {
+            using (TContext db= new TContext())
+            {
+                var deletedEntity = db.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                db.SaveChanges();
+            }
+        }
+
+        public Tentity Get(Expression<Func<Tentity, bool>> filter)
+        {
+            using (TContext db= new TContext())
+            {
+                return db.Set<Tentity>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Tentity> GetAll(Expression<Func<Tentity, bool>> filter = null)
+        {
+            using (TContext db= new TContext())
+            {
+                return filter == null
+                    ? db.Set<Tentity>().ToList()
+                    : db.Set<Tentity>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Tentity entity)
+        {
+            using (TContext db= new TContext())
+            {
+                var updatedEntity = db.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+    }
+}
