ada32af7e14ee744336b595a2a8811fa815984de
mehmet ali <alimehmet1@windowslive.com>
2022-01-10 16:06:04 +0300

    commit

---
+++ BusinessLayer/Abstract/IAdminService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Utilities.Response;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IAdminService
+    {
+        IResponse Add(Admin q);
+        IResponse Delete(Admin q);
+        IResponse Update(Admin q);
+        IDataResponse<List<Admin>> GetAll(Expression<Func<About, bool>> filter = null);
+        IDataResponse<Admin> GetById(int id);
+    }
+}
---
+++ BusinessLayer/Concrete/AdminManager.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLayer.Abstract;
+using Core.Utilities.Response;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AdminManager : IAdminService
+    {
+        private IAdminDal _adminDal;
+
+        public AdminManager(IAdminDal adminDal)
+        {
+            _adminDal = adminDal;
+        }
+
+        public IResponse Add(Admin q)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResponse Delete(Admin q)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResponse<List<Admin>> GetAll(Expression<Func<About, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResponse<Admin> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResponse Update(Admin q)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
